610af9b403fa6f4448693632b0cee87d37a340b6
Unknown <atiagoferreira@gmail.com>
2017-12-13 13:29:33 -0200

    CadastroAlunos
    Commit projeto.

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+
+        public ActionResult CadastroAluno()
+        {
+            ModelAluno objAluno = new ModelAluno();
+
+            return View(objAluno);
+        }
+
+        [HttpPost]
+        public ActionResult ExibeDadosCadastroAluno(FormCollection form)
+        {
+            ViewBag.IdAluno = form["id_Aluno"];
+            ViewBag.Nome = form["Nome_Aluno"];
+            ViewBag.Email = form["Email_Aluno"];
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ExibeDadosCadastroAlunoTipado(ModelAluno aluno)
+        {
+            return View(aluno);
+        }
+
+        public ActionResult Menu()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ MvcApplication1/Models/ModelAluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class ModelAluno
+    {
+        public int id_Aluno { get; set; }
+
+        public string Nome_Aluno { get; set; }
+
+        public string Email_Aluno { get; set; }
+    }
+}
